446c2bef9932781fd5618fe1cfd33e3b7f16b07c
System Administrator <root@iwabuchikeigonoMacBook-puro.local>
2019-03-09 17:55:58 +0900

    make admin setting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # https://qiita.com/awakia/items/3265f91768bcfd582bd7 を参照。親レイアウトにapplicationを選択した
+  def parent_layout(layout)
+    @view_flow.append(:layout, self.output_buffer)
+    self.output_buffer = render(file: "layouts/#{layout}")
+  end
 end
